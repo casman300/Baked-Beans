8054f8f489177a57d8823683b30c06c61690709f
Toby Seaman <turbotas@yahoo.com>
2018-01-13 19:34:31 +0000

    First try at populating results page using the template.

---
+++ inc/dbsettings-demo.inc.php
@@ -0,0 +1,12 @@
+<?php 
+# This is the database configuration file
+# This file comtains the sensitive parametersi
+# that should not be committed to git.
+
+$dbhost="localhost";
+$dbport="3306";
+$dbuser="USER";
+$dbpass="PASSWORD";
+$dbname="bakedbeanv1";
+
+?>
