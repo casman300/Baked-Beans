e187da1076e9c9155229257bbf8276dae7ebe787
Charlie Seaman <charlie@casman.co.uk>
2022-10-30 20:02:38 +0000

    Fixed pages to use includes files in new locations

--- RecordDisplay.php
+++ RecordDisplay.php
@@ -2,76 +2,27 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Charlie Seaman">
-
-    <title>Baked Bean Project - Individual Data: BLANK</title>
-	<link rel="icon" type="image/png" href="favicon.ico">
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="css/clean-blog.css" rel="stylesheet">
-
-    <!-- Custom Fonts -->
-    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href='http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
+    <?php  
+        $pagename = "Record Display";
+        $pagedescription = "";
+        include('includes/header.php');
+    ?>
 </head>
 
 <body>
-    <!-- Navigation -->
-    <nav class="navbar navbar-default navbar-custom navbar-fixed-top navbar-dark">
-        <div class="container-fluid">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header page-scroll">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="../index.html">Baked Beans</a>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
-                    <li>
-                        <a href="../index.html">Home / About</a>
-                    </li>
-                    <li>
-                        <a href="../graphs.html">Graphs</a>
-                    </li>
-                    <li>
-                        <a href="../data-pages/index.html">Raw Data</a>
-                    </li>
-                    <li>
-                        <a href="../contact.html">Contact</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
+    <?php  
+        include('includes/menu.php');
+    ?>
 
 <?php 
 
-include 'inc/configuration.php';
+include 'includes/configuration.php';
 
-$id = intval(htmlspecialchars($_GET["id"]));
+if (isset($_GET["id"])) {
+    $id = sanitizeString($_GET["id"]);
+} else {
+    $id = "";
+}
 
 // Create connection
 $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
@@ -128,7 +79,7 @@ if ($result->num_rows > 0) {
 //       Dont have this stored yet
 //      <tr><th scope="row">Percentage of beans to liquid</th><td><?php echo $row["beanstoliquid"]
         ?>
-		<tr><th scope="row">Percentage of beans to liquid</th><td><?php echo $percb . ":" . $percj; ?></td></tr>
+		<tr><th scope="row">Percentage of beans to liquid</th><td><?php echo $percb . "% / " . $percj; ?>%</td></tr>
 
         <tr><th scope="row">Bean count</th><td><?php echo $row["beancount"]?></td></tr>
         <tr><th scope="row">Taste rating</th><td><?php echo $row["tasterating"]?></td></tr>
@@ -152,38 +103,17 @@ if ($result->num_rows > 0) {
         
         <div class="container">
         <div class="page-header">
-        <h1>Individual Results <small><?php echo $row["productname"]?></small></h1>
-</div>
-	<div class="row">
-		<div class="col-xs-12">
-			<a href="#" class="thumbnail">
-				<img src="..." alt="...">
-			</a>
-			<a href="#" class="thumbnail">
-				<img src="..." alt="...">
-			</a>
-		</div>
-	</div>
-        
+        <h1>Individual Results <small>Test Not Found!</small></h1>
+</div>  
     0 results: We don't have a record of that test, or maybe it's not published yet.
     <?php
     }
 $conn->close();
 ?>
 	
-    <footer>
-                    <p class="copyright text-muted">Copyright &copy; Charlie Seaman 2018 <a href="../humans.txt"><img src="../img/humanstxt.gif" alt="Humans.txt" width="88" height="31"></a></p>
-    </footer>
-
-    <!-- jQuery -->
-    <script src="../js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="../js/bootstrap.min.js"></script>
-
-    <!-- Custom Theme JavaScript -->
-    <script src="../js/clean-blog.min.js"></script>
-
+    <?php  
+        include('includes/footer.php');
+    ?>
 </body>
 
 </html>
